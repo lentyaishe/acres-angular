7c71d3c6798c1dece51bc9ccfdded2232ec24556
Ilya Pogrebensky <lentyai@gmail.com>
2023-02-09 17:16:54 +0200

    Angular basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticlesModule } from './modules/articles/articles.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ArticlesModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/modules/articles/components/article/article.component.ts
@@ -0,0 +1,65 @@
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, QueryList, ViewChild, ViewChildren } from "@angular/core";
+import { Paragraph } from "../../models/paragraph";
+import { ParagraphComponent } from "../paragraph/paragraph.component";
+
+enum ArticleStatus {
+    Read = "read",
+    Unread = "unread"
+}
+
+@Component({
+    selector: "aac-article",
+    templateUrl: "./article.component.html",
+    styleUrls: ["./article.component.less"]
+})
+export class ArticleComponent implements OnInit, AfterViewInit {
+    @Input() date: Date | undefined = undefined;
+    @Input() title: string | undefined = "";
+    @Input() isRead: boolean | undefined = undefined;
+    @Input() subTitle: string | undefined = "";
+    @Input() paragraphs: Paragraph[] | undefined = undefined;
+
+    @Output() isReadChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+    @Output() titleClicked: EventEmitter<void> = new EventEmitter<void>();
+
+    @ViewChild("titleElement") titleElement: ElementRef | undefined = undefined;
+    @ViewChildren(ParagraphComponent) paragraphElements: QueryList<ParagraphComponent> | undefined = undefined;
+
+    public ArticleStatus = ArticleStatus;
+
+    // public firstParagraph: Paragraph | null = null;
+
+    constructor() {
+        // console.log("Constructor: title:", this.title); // title is still "" which is the default value
+    }
+
+    public ngOnInit(): void {
+        // console.log("OnInit: title:", this.title); // here title gets the actual value pased from outside
+        // if (this.paragraphs && this.paragraphs.length > 0) {
+        //     this.firstParagraph = this.paragraphs[0];
+        // }
+
+        console.log("OnInit: title:", this.titleElement)
+    }
+    
+    public ngAfterViewInit(): void {
+        console.log("AfterViewInit: title:", this.titleElement)
+    }
+
+    public onIsReadChanged(): void {
+        this.isReadChange.emit(this.isRead);
+    }
+
+    public onTitleClicked(): void {
+        this.titleClicked.emit();
+
+        const titleElem: HTMLSpanElement = this.titleElement?.nativeElement;
+        titleElem.style.color = "blue";
+
+        // console.log(titleElem.classList);
+
+        this.paragraphElements?.forEach((paragraphElement: ParagraphComponent) => {
+            console.log(paragraphElement);
+        });
+    }
+}
