7dda3e34f6e3f5b4fd03d593b4e8755797ee259c
Ilya Pogrebensky <ilya@billboy.co.il>
2023-02-15 13:59:59 +0200

    Generic option chooser component complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ISelectableOption } from './modules/core/models/selectable-option';
 import { Component } from "@angular/core";
 import { Paragraph } from "./modules/articles/models/paragraph";
 import { LocalStorageService } from "./modules/core/services/local-storage.service";
@@ -20,29 +21,79 @@ interface Article extends ArticleBase {
   styleUrls: ["./app.component.less"]
 })
 export class AppComponent {
-    public articles: Article[] = [];
+  public JSON = JSON;
+  public articles: Article[] = [];
+
+  public optionsSet1: ISelectableOption<number>[] = [];
+  public optionsSet2: ISelectableOption<ArticleBase>[] = [];
+
+  public selectedOption1: number[] | number | null = [1,2];
+  public selectedOption2: ArticleBase[] | ArticleBase | null = [{
+          id: 1,
+          isRead: true
+        }, {
+          id: 3,
+          isRead: true
+        }];
 
     constructor(
         private localStorageService: LocalStorageService
     ) {
-        this.articles.push({
-            id: 1,
-            date: new Date(),
-            title: "Acres course",
-            isRead: false,
-            subTitle: "The article about the course",
-            paragraphs: [
-                {
-                    title: "Lesson 1",
-                    body: "HTML / CSS",
-                    isRemote: true
-                }, {
-                    title: "Lesson 2",
-                    body: "Typescript",
-                    isRemote: false
-                }
-            ]
-        });
+      this.articles.push({
+          id: 1,
+          date: new Date(),
+          title: "Acres course",
+          isRead: false,
+          subTitle: "The article about the course",
+          paragraphs: [
+              {
+                  title: "Lesson 1",
+                  body: "HTML / CSS",
+                  isRemote: true
+              }, {
+                  title: "Lesson 2",
+                  body: "Typescript",
+                  isRemote: false
+              }
+          ]
+      });
+
+      this.optionsSet1.push({
+        title: "Option 1",
+        value: 1
+      });
+
+      this.optionsSet1.push({
+        title: "Option 2",
+        value: 2
+      });
+
+
+      this.optionsSet2.push({
+        title: "Article 1",
+        value: {
+          id: 1,
+          isRead: true
+        }
+      });
+
+      this.optionsSet2.push({
+        title: "Article 2",
+        value: {
+          id: 2,
+          isRead: false
+        }
+      });
+
+      this.optionsSet2.push({
+        title: "Article 3",
+        value: {
+          id: 3,
+          isRead: true
+        }
+      });
+
+      // this.selectedOption2 = this.optionsSet2[1].value as ArticleBase;
     }
 
     public onTitleClicked(): void {
--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,10 +1,18 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { OptionChooserComponent } from './components/option-chooser/option-chooser.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OptionChooserComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    OptionChooserComponent
   ]
 })
 export class CoreModule { }
